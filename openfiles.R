0ffc57e5b99f945a3033f536ff494b7b18c379fb
frowningw <b04303073@g.ntu.edu.tw>
2021-01-21 20:28:45 +0800

    add comments change numeric(0) to vector("numeric",0)

--- openfiles.R
+++ openfiles.R
@@ -5,8 +5,8 @@ setwd(dir = "/Users/will/Desktop/specdata")
 
 pollutantmean <- function(pollutant, id = 1:332) {
   len <- length(id)
-  sumvalue <- numeric(0)
-  howmany <- numeric(0)
+  sumvalue <- vector("numeric",0)
+  howmany <- vector("numeric",0)
   for (i in id) {
     if (i < 10) { filename<- paste("00", i, ".csv", sep = "")} 
     else if (10 <= i & i < 100) {filename <- paste("0", i, ".csv", sep = "")} 
